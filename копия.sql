a2f638b76d54bfc009be1b9c932fcdecfa207461
Terminator34212199 <161671082+Terminator34212199@users.noreply.github.com>
2025-03-17 18:29:57 +0700

    Update копия.sql

--- копия.sql
+++ копия.sql
@@ -22,7 +22,7 @@
 DROP TABLE IF EXISTS `time`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `time` (
+CREATE TABLE `timeeeee` (
   `id` int NOT NULL AUTO_INCREMENT,
   `specialnost` varchar(50) NOT NULL,
   `dataa` date NOT NULL,
